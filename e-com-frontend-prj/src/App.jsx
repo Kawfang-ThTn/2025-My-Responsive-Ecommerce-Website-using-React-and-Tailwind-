7fc662e45192f87e056157f69e4d2a04ce32392c
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2025-01-21 16:15:09 +0700

    starting nav

--- e-com-frontend-prj/src/App.jsx
+++ e-com-frontend-prj/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 const App = () => {
   return (
+    <>
     <div>App</div>
+    <Navbar />
+    </>
   )
 }
+
 
 export default App
