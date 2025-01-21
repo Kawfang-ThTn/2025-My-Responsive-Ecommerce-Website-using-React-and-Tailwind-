830abe53eb39d55dbd6c21ec45530898feed4a49
KawfangThTn <thanthong.somwijit@gmail.com>
2025-01-22 00:54:19 +0700

    finish nav and add asset darkmode

--- e-com-frontend-prj/src/components/Navbar/Navbar.jsx
+++ e-com-frontend-prj/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
 const MenuLinks = [
   {
@@ -21,16 +24,15 @@ const MenuLinks = [
     name: "Blogs",
     link: "/#blogs",
   },
-]
+];
 
 function Navbar() {
   return (
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
         <div className="py-4">
-
-          <div className="container">
-            <div>
+          <div className="container flex justify-between items-center ">
+            <div className="flex items-center gap-4">
               <a
                 href="#"
                 className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
@@ -38,21 +40,52 @@ function Navbar() {
                 TT-Shop
               </a>
 
-            <div>
-              <ul>
-                {
-                  MenuLinks.map((data, index) => (
-                    <li key={index} >
-                      <a href={data.link}>{data.name}</a>
+              <div className="hidden lg:block">
+                <ul className="flex items-center gap-4 ">
+                  {MenuLinks.map((data, index) => (
+                    <li key={index}>
+                      <a
+                        href={data.link}
+                        className="inline-block px-4 font-semibold text-gray-500
+                      hover:text-black
+                      dark:hover:text-white duration-200"
+                      >
+                        {data.name}
+                      </a>
                     </li>
-                  ))
-                }
-              </ul>
+                  ))}
+                </ul>
+              </div>
             </div>
 
+            <div className="flex justify-between items-center gap-4 ">
+              <div className="relative hidden sm:block">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="relative  z-10 bg-transparent w-12 h-12 rounded-full cursor-pointer pl-12 border border-gray-200 dark:border-gray-700
+                focus:w-full focus:border-gray-100 focus:cursor-text focus:pl-16 focus:pr-4"
+                />
+                <IoMdSearch
+                  className="absolute inset-y-0 my-auto h-8 w-12 px-3  text-xl text-gray-600 border-r border-transparent peer-focus:border-gray-100
+                dark:text-gray-400 "
+                />
+              </div>
+              <div className="flex items-center gap-4"> 
+                <button className="relative p-3">
+                  <FaCartShopping 
+                  className="text-xl text-gray-600 dark:text-gray-400" 
+                  />
+                  <div className="absolute -top-0.5 -right-0.5 bg-primary text-white w-4 h-4 rounded-full flex items-center justify-center">
+                    4
+                  </div>
+                </button>
+                <div>
+                  <DarkMode />
+                </div>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
